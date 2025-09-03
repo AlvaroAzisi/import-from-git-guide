a7ec53776d2b881bcd883d69f63d58046c648ee7
Anomaly <anomaly.anomaly.exe@gmail.com>
2025-09-03 07:11:54 +0000

    ganti supabaseURL

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ipnffgyvfwtrtxskwlxp.supabase.co'
+const supabaseUrl = 'https://vswctyplzrwjgnwxplhg.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwbmZmZ3l2Znd0cnR4c2t3bHhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxMzk3MDUsImV4cCI6MjA2ODcxNTcwNX0.5VpgBqKetQeFE7qQ_aZOeQyBj7h3YjRgDAhaOJvMFZM'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
